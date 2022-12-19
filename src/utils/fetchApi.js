99e14046e1a4b144ea4c1132e3c332600ff30b76
MangaiyarkarasiM <mangaivmm@gmail.com>
2022-12-19 09:18:35 +0530

    endpoint update

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchApi = axios.create({
-    baseURL: 'https://book-buddy-lib.herokuapp.com'
+    baseURL: 'https://book-buddy.vercel.app'
   //baseURL: 'http://localhost:5000',
 })
 
